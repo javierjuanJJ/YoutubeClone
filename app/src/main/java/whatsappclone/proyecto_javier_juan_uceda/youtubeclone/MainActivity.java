56000986a7f1de69a2be7c18dbc09e35a2247e96
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-01 12:02:46 +0100

    Youtube Clone | Google Sign In Auth Firebase | Android Studio | Part 6

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/MainActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/MainActivity.java
@@ -1,20 +1,43 @@
 package whatsappclone.proyecto_javier_juan_uceda.youtubeclone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowInsets;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import whatsappclone.proyecto_javier_juan_uceda.youtubeclone.fragments.ExploreFragment;
 import whatsappclone.proyecto_javier_juan_uceda.youtubeclone.fragments.HomeFragment;
@@ -26,7 +49,9 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
     private Toolbar toolbar;
     private BottomNavigationView bottomNavigationView;
     FrameLayout frameLayout;
-
+    private ImageView userProfileImage;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +69,101 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         frameLayout = findViewById(R.id.frame_layout);
 
+        userProfileImage = findViewById(R.id.user_profile_image);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "User alreasy sign in", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    showDialogue();
+                }
+            }
+        });
+
         getSupportActionBar().setTitle("");
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    private void showDialogue() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+        ViewGroup viewGroup = findViewById(androidx.appcompat.R.id.content);
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_signin_dialogue, viewGroup, false);
+        builder.setView(view);
+
+        TextView txtGoogleSignIn = view.findViewById(R.id.txt_google_signIn);
+        txtGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        builder.create().show();
+
+    }
+
+
+
+    private GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
+
+    private GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    private static final int RC_SIGN_IN = 100;
+
+    private void signIn() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("username", account.getDisplayName());
+                            map.put("email", account.getEmail());
+                            map.put("profile", String.valueOf(account.getPhotoUrl()));
+                            map.put("uid", firebaseUser.getUid());
+                            map.put("search", account.getDisplayName().toLowerCase());
+
+                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+                            reference.child(firebaseUser.getUid()).setValue(map);
+
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } catch (ApiException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
     }
 
     @Override
@@ -64,8 +180,6 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
                 break;
             case R.id.search:
                 Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();break;
-            case R.id.account:
-                Toast.makeText(this, "account", Toast.LENGTH_SHORT).show();break;
             default:
                 return super.onOptionsItemSelected(item);
         }
