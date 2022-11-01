c8811874ee7f37fb6677b1ccd163ca6b03b9b603
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-01 14:19:01 +0100

    Youtube Clone | Channel Create On Firebase | Android Studio | Part 10

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/AccountActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/AccountActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +23,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,6 +39,7 @@ public class AccountActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseUser user;
     private DatabaseReference reference;
+    private String profileValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,7 +108,60 @@ public class AccountActivity extends AppCompatActivity {
         dialog.setCancelable(true);
         dialog.setCanceledOnTouchOutside(true);
 
+        EditText input_channel_name = dialog.findViewById(R.id.input_channel_name);
+        EditText input_description = dialog.findViewById(R.id.input_description);
+        EditText txt_create_channel = dialog.findViewById(R.id.txt_create_channel);
+
+        txt_create_channel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = input_channel_name.getText().toString();
+                String description = input_description.getText().toString();
+
+                if (name.isEmpty() || description.isEmpty()){
+                    Toast.makeText(AccountActivity.this, "Fill required fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    createNewChannel(name, description, dialog);
+                }
+            }
+        });
+
         dialog.show();
+    }
+
+    private void createNewChannel(String name, String description, Dialog dialog) {
+        ProgressDialog progressDialog = new ProgressDialog(AccountActivity.this);
+        progressDialog.setTitle("New Channel");
+        progressDialog.setMessage("Creating ...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        String date = DateFormat.getDateInstance().format(new Date());
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("channel_name", name);
+        map.put("description", description);
+        map.put("joined", date);
+        map.put("uid", user.getUid());
+        map.put("channel_logo", profileValue );
+
+        reference.child("Channel").child(user.getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    progressDialog.dismiss();
+                    dialog.dismiss();
+                    Toast.makeText(AccountActivity.this, name + " channel has been created", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    progressDialog.dismiss();
+                    dialog.dismiss();
+                    Toast.makeText(AccountActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 
     private void getData() {
@@ -109,7 +171,7 @@ public class AccountActivity extends AppCompatActivity {
                 if (snapshot.exists()){
                     String usernameValue = snapshot.child("username").getValue().toString();
                     String emailValue = snapshot.child("email").getValue().toString();
-                    String profileValue = snapshot.child("profile").getValue().toString();
+                    profileValue = snapshot.child("profile").getValue().toString();
 
                     username.setText(usernameValue);
                     email.setText(emailValue);
