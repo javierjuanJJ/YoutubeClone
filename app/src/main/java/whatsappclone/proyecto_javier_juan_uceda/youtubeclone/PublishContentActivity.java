65ffe089c1df0dd9a1b98e8d93fa91c9c99b5f99
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-03 09:52:31 +0100

    Youtube Clone | Create Playlist Dialog | Android Studio | Part 17

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/PublishContentActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/PublishContentActivity.java
@@ -2,11 +2,18 @@ package whatsappclone.proyecto_javier_juan_uceda.youtubeclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import com.google.android.material.chip.Chip;
 
 public class PublishContentActivity extends AppCompatActivity {
 
@@ -14,6 +21,9 @@ public class PublishContentActivity extends AppCompatActivity {
     private String type;
     private Uri videoUri;
     private MediaController mediaController;
+    private EditText chip;
+    private TextView txtUploadVideo, txtChoosePlaylist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,9 @@ public class PublishContentActivity extends AppCompatActivity {
     private void setUI() {
         videoView = findViewById(R.id.videoView);
         mediaController = new MediaController(PublishContentActivity.this);
-
+        chip = findViewById(R.id.inputVideoTag);
+        txtUploadVideo = findViewById(R.id.txt_upload_video);
+        txtChoosePlaylist = findViewById(R.id.choosePlayList);
         Intent intent = getIntent();
 
         if (intent != null) {
@@ -35,5 +47,22 @@ public class PublishContentActivity extends AppCompatActivity {
             videoView.setMediaController(mediaController);
             videoView.start();
         }
+
+        txtChoosePlaylist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPlayListDialogue();
+            }
+        });
+    }
+
+    Dialog dialog;
+
+    private void showPlayListDialogue() {
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.playlist_dialog);
+        dialog.setCancelable(true);
+        dialog.show();
     }
 }
