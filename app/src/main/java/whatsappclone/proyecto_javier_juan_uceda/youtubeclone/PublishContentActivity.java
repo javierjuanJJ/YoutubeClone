1730b06fc1d155a77ee119ab4ceba19c97d91db1
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-03 10:04:25 +0100

    Youtube Clone | Create Playlist On Firebase | Android Studio | Part 18

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/PublishContentActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/youtubeclone/PublishContentActivity.java
@@ -1,6 +1,8 @@
 package whatsappclone.proyecto_javier_juan_uceda.youtubeclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.content.Intent;
@@ -11,9 +13,20 @@ import android.view.Window;
 import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.google.android.material.chip.Chip;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class PublishContentActivity extends AppCompatActivity {
 
@@ -63,6 +76,71 @@ public class PublishContentActivity extends AppCompatActivity {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.playlist_dialog);
         dialog.setCancelable(true);
+        
+        EditText input_playlist_name = dialog.findViewById(R.id.input_playlist_name);
+        TextView txt_add = dialog.findViewById(R.id.txt_add);
+        RecyclerView recyclerView = dialog.findViewById(R.id.recyclerView);
+        
+        checkUserAlreadyHavePlaylist(recyclerView);
+        
+        txt_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = input_playlist_name.getText().toString();
+                if (value.isEmpty()){
+                    Toast.makeText(PublishContentActivity.this, "Enter Playlist name", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    createPlayList(value);
+                }
+                    
+            }
+        });
+        
         dialog.show();
+    }
+
+    private void createPlayList(String value) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Playlists");
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("playlist_name", value);
+        map.put("videos",0);
+        map.put("uid",user.getUid());
+        
+        reference.child(user.getUid()).child(value).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(PublishContentActivity.this, "New playlist created", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(PublishContentActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void checkUserAlreadyHavePlaylist(RecyclerView recyclerView) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Playlist");
+        reference.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    Toast.makeText(PublishContentActivity.this, "User have playlists", Toast.LENGTH_SHORT).show();
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    recyclerView.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(PublishContentActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
